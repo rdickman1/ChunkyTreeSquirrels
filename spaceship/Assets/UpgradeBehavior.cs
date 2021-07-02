25fff068bd0fef4cd037e358a2f0ae7b708dc5ba
Ryan Dickman <rdickman1@gmail.com>
2021-07-02 11:14:27 -0500

    Added multiple upgrade functionality

--- spaceship/Assets/UpgradeBehavior.cs
+++ spaceship/Assets/UpgradeBehavior.cs
@@ -5,18 +5,50 @@ using UnityEngine;
 public class UpgradeBehavior : MonoBehaviour
 {
 
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+   
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.transform.tag == "Upgrade")
+        //Gun Upgrade
+        if(collider.transform.tag == "Gun Upgrade")
         {
             //Run into upgrade
+
+            //Upgrade weapons
+
+            //Destroy Upgrade object
+            Destroy(collider.gameObject);
+        }
+
+        //Shield Upgrade
+        if (collider.transform.tag == "Shield Upgrade")
+        {
+            //Run into upgrade
+
+            //Upgrade Shield
+
+            //Destroy Upgrade object
+            Destroy(collider.gameObject);
+        }
+
+        //Melee Upgrade
+        if (collider.transform.tag == "Melee Upgrade")
+        {
+            //Run into upgrade
+
+            //Upgrade melee weapon
+
+            //Destroy Upgrade object
+            Destroy(collider.gameObject);
+        }
+
+        //Health Upgrade
+        if (collider.transform.tag == "Health Upgrade")
+        {
+            //Run into upgrade
+
+            //Upgrade health
+
+            //Destroy Upgrade object
             Destroy(collider.gameObject);
         }
     }
